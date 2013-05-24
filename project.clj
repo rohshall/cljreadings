86d968aa5fdab7b6a972c25c797607db39b901e3
Salil Wadnerkar <rohshall@gmail.com>
2013-05-24 09:46:05 +0800

    Added compojure template

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject cljreadings "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "REST based device readings service"
+  :url "http://github.com/rohshall/cljreadings"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main cljreadings.core)
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.3"]]
+  :ring {:handler cljreadings.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})

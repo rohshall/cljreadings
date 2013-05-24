1e51a045466d0ff20060a9944bc6e5c9f5a0ace2
Salil Wadnerkar <rohshall@gmail.com>
2013-05-24 23:54:15 +0800

    Added composure routes and clojure jdbc calls

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [org.clojure/java.jdbc "0.3.0-alpha4"]
+                 [org.postgresql/postgresql "9.2-1002-jdbc4"]
+                 [org.apache.tomcat/tomcat-dbcp "7.0.40"]
+                 [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler cljreadings.handler/app}
   :profiles
--- src/cljreadings/handler.clj
+++ src/cljreadings/handler.clj
@@ -1,13 +1,88 @@
 (ns cljreadings.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.java.jdbc :as jdbc]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.json :refer [wrap-json-params]]
+            ))
+
+(def db-spec {:classname "org.postgresql.Driver"
+              :subprotocol "postgresql"
+              :subname "sd_ventures_development"
+              :user "sd_ventures"
+              :password ""})
+
+
+(defn get-devices []
+  (jdbc/with-connection db-spec
+    (jdbc/with-query-results rs ["SELECT * FROM devices"]
+      (doall rs))))
+
+
+(defn str-to-timestamp [s]
+  (let [date-format (java.text.SimpleDateFormat. "yyyy-MM-dd")
+        parse-pos (java.text.ParsePosition. 0)]
+    (java.sql.Timestamp. (.getTime (.parse date-format s parse-pos)))))
+
+
+(defn create-device [{:keys [mac_addr device_type_id manufactured_at created_at] :as device-info}]
+  (println "creating device " device-info)
+  (jdbc/with-connection db-spec
+    (jdbc/insert-record :devices
+      {:mac_addr mac_addr :device_type_id device_type_id :manufactured_at (str-to-timestamp manufactured_at)})))
+
+
+(defn get-device [device-id]
+  (jdbc/with-connection db-spec
+    (jdbc/with-query-results res ["SELECT * FROM devices WHERE mac_addr = ?" device-id]
+      (doall res))))
+
+
+(defn get-device-readings [device-id {:keys [from to]}]
+  (let [sql-params 
+        (cond 
+          (and from to) ["SELECT * FROM readings WHERE device_mac_addr = ? AND created_at BETWEEN ? AND ?"
+                         device-id (str-to-timestamp from) (str-to-timestamp to)]
+          from ["SELECT * FROM readings WHERE device_mac_addr = ? AND created_at > ?"
+                device-id (str-to-timestamp from)]
+          to ["SELECT * FROM readings WHERE device_mac_addr = ? AND created_at < ?"
+              device-id (str-to-timestamp to)]
+          :else ["SELECT * FROM readings WHERE device_mac_addr = ?" device-id])]
+    (jdbc/with-connection db-spec
+      (jdbc/with-query-results res sql-params
+        (doall res)))))
+
+
+(defn create-device-reading [device-id {:keys [value]}]
+  (jdbc/with-connection db-spec
+    (jdbc/insert-record :readings
+      {:device_mac_addr device-id :value value :created_at (java.sql.Timestamp. (.getTime (java.util.Date.)))})))
 
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
   (route/resources "/")
+  (context "/api/1" []
+           (defroutes api-routes
+             (GET "/devices" [] (get-devices))
+             (POST "/devices" {device-info :params} (create-device device-info))
+             (context "/devices/:device-id" [device-id]
+                      (defroutes device-routes
+                        (GET "/" [] (get-device device-id))
+                        (GET "/readings" {params :params} (get-device-readings device-id params))
+                        (POST "/readings" {reading-info :params} (create-device-reading device-id reading-info))))))
+
   (route/not-found "Not Found"))
 
+
 (def app
-  (handler/site app-routes))
+  (->
+  (handler/api app-routes)
+    (wrap-json-params)))
+
+
+(def war-handler
+  (->
+    app
+    (wrap-resource "public")))
+
